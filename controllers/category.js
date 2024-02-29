3a85058012e78cc020971e2dd3d30b69c4004c1b
Ibrahim Gaber <ibrahimseda322@gmail.com>
2024-02-29 21:59:48 +0200

    book,user,remove console logs

--- controllers/category.js
+++ controllers/category.js
@@ -2,7 +2,6 @@ import Category from "../models/category.js";
 import Book from "../models/book.js";
 
 export const createCategory = async (req, res) => {
-  console.log(req.body);
   try {
     const category = await Category.create(req.body);
     res.json(category);
@@ -25,7 +24,6 @@ export const getAllCategories = async (req, res) => {
     }
 
     const categories = await Category.find().limit(Limit).skip(Skip);
-    console.log(categoriesCount);
     res.json({ categories, categoriesCount });
   } catch (error) {
     res.status(500).json({ message: error.message });
