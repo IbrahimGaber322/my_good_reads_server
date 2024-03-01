d288482807476189ef718e827c2559414121256a
Ibrahim Gaber <ibrahimseda322@gmail.com>
2024-03-01 22:21:07 +0200

    cors problem

--- index.js
+++ index.js
@@ -11,8 +11,6 @@ const DB = process.env.DB;
 const FRONT_URL = process.env.FRONT_URL;
 
 const app = express();
-app.use(express.json({ limit: "50mb" }));
-app.use("/uploads", express.static("uploads"));
 app.use(cors());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
@@ -24,6 +22,8 @@ app.use((req, res, next) => {
   );
   next();
 });
+app.use(express.json({ limit: "50mb" }));
+app.use("/uploads", express.static("uploads"));
 app.use("/", routes);
 
 mongoose
