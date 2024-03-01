002793e275f838aef92785f2c1ecf5b416a5c1ce
Ibrahim Gaber <ibrahimseda322@gmail.com>
2024-03-01 17:54:03 +0200

    node fetch

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import "./config/fetch-polyfill.js";
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
@@ -11,10 +12,9 @@ const DB = process.env.DB;
 const FRONT_URL = process.env.FRONT_URL;
 
 const app = express();
-app.use(express.json({ limit: '50mb' }));
-app.use('/uploads', express.static('uploads'));
+app.use(express.json({ limit: "50mb" }));
+app.use("/uploads", express.static("uploads"));
 app.use(cors());
-
 
 app.use("/", routes);
 
