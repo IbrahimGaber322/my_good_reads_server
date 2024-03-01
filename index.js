e3011716d3ebda18c1e89cb373043be8f84d6b89
Ibrahim Gaber <ibrahimseda322@gmail.com>
2024-03-01 17:42:33 +0200

    allow all cors

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const FRONT_URL = process.env.FRONT_URL;
 const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use('/uploads', express.static('uploads'));
-app.use(cors());
+app.use(cors('*'));
 
 
 app.use("/", routes);
