ee41ca85fbd6fa25d0d5bc025bd5ea8724dcc091
Ibrahim Gaber <ibrahimseda322@gmail.com>
2024-03-09 13:55:42 +0200

    delete book

--- controllers/book.js
+++ controllers/book.js
@@ -127,12 +127,6 @@ export const deleteBook = async (req, res) => {
     if (!deletedBook) {
       return res.status(404).json({ message: "Book not found" });
     }
-    const imageUrl = deletedBook.image;
-    if (imageUrl) {
-      const parsedUrl = new URL(imageUrl);
-      const pathAfterHostname = parsedUrl.pathname;
-      await fs.unlink(pathAfterHostname.slice(1));
-    }
     res.status(200).json({ message: "Book deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
